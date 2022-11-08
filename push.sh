69e962e420133d26b08b557df72d0556c8e4d190
liuhuiliang <825378291@qq.com>
2022-11-08 23:11:31 +0800

    拆分

--- push.sh
+++ push.sh
@@ -1,5 +1,13 @@
+#./gradlew arm64:assembleDebug
+#./gradlew arm64:publishPluginPublicationToNexusRepository
+#./gradlew armeabi:assembleDebug
+#./gradlew armeabi:publishPluginPublicationToNexusRepository
+#./gradlew armeabi_v7:assembleDebug
+#./gradlew armeabi_v7:publishPluginPublicationToNexusRepository
 ./gradlew media:assembleDebug
 ./gradlew media:publishPluginPublicationToNexusRepository
+#./gradlew x86:assembleDebug
+#./gradlew x86:publishPluginPublicationToNexusRepository
 p=`pwd`
 cd /Users/liuhuiliang/work/gitMaven
 echo ".DS_Store" > .gitignore
